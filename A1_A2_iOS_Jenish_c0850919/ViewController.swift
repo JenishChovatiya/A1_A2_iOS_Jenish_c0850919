6558773d3b57f2004129dd09df141eb77b566fc9
c0850919 <c0850919@mylambton.ca>
2022-05-25 22:47:22 -0400

    Working on Path and Triangle  for adding Polygon.

--- A1_A2_iOS_Jenish_c0850919/ViewController.swift
+++ A1_A2_iOS_Jenish_c0850919/ViewController.swift
@@ -11,28 +11,28 @@ import CoreLocation
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+   
+    //connecting all the items
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var userMapView: MKMapView!
     
-  
-    @IBAction func goBtn(_ sender: Any)
-    {
-       
-    }
+
     	
     var arrayCity: [MKMapItem] = []
     
     //applay location manager
-    let locationManager = CLLocationManager()
+    var locationManager: CLLocationManager!
     
-    var destination: CLLocationCoordinate2D!
-    
+    let thingg = UINavigationItem()
     
     var addpolygon : MKPolygon? = nil
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        locationManager = CLLocationManager()
         locationManager.delegate = self
         
         
@@ -41,22 +41,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         
         //for automatically updating user location
         locationManager.startUpdatingLocation()
-        
-        
-        func insertingPolygon()
-        {
-            var points: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
-            
-            for i in 0..<arrayCity.count
-            {
-                points.append(arrayCity[i].placemark.coordinate)
-            }
-            let polygon = MKPolygon(coordinates: points, count: points.count)
-            self.addpolygon = polygon
-            userMapView.addOverlay(polygon)
-            
-            
-        }
         
         
         //double  tap
@@ -69,103 +53,57 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         //when user longpressed on screen
         let userLongPressedRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(whenpressed))
         self.userMapView.addGestureRecognizer(userLongPressedRecognizer)
-    	}
-
+        
+        thingg.rightBarButtonItem = UIBarButtonItem(title: "GO", style: .plain, target: self, action: #selector(whentappedonpho))
+        self.navigationBar.items = [thingg]
+        
+    }//view did load over here
     
-    //doubletap gesture callout
-    @objc func doubletaped(sender: UITapGestureRecognizer)
+    
+    override func viewWillAppear(_ animated: Bool)
     {
-        print("double tap gesture recognized")
-        let touchPoint = sender.location(in: userMapView)
-                        let coordinate = userMapView.convert(touchPoint, toCoordinateFrom: userMapView)
-                        let annotation = MKPointAnnotation()
-                        annotation.title = "B"
-                        annotation.coordinate = coordinate
-                        userMapView.addAnnotation(annotation)
+        checkforRouteOption()
     }
     
-
-    @objc func whenpressed(sender: UILongPressGestureRecognizer)
-        {
-            //alert message for displaying want to add or not ?
-            
-            let alert = UIAlertController(title: "Want to add this location", message: "Add !!", preferredStyle: .alert)
-            
-            
-            //when user will press yes than it will redirectuser to another page for searching city name
-            
-            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { [self] (action: UIAlertAction!)
-                in
-                let searcscreen = self.storyboard?.instantiateViewController(withIdentifier: "AddyourCityVcViewController") as! AddyourCityVcViewController
-                searcscreen.viewofMap = self.userMapView
-                searcscreen.delegate = self
-                self.navigationController?.pushViewController(searcscreen, animated: true)
-                
-            }))
-            
-            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (action: UIAlertAction!) in }))
-            
-            present(alert, animated: true, completion: nil)
+    func checkforRouteOption() {
+        if arrayCity.count > 2 {
+            self.navigationBar.items = [thingg]
         }
-    
-}
-
-//create extenstion for viewControllar
-extension ViewController: CLLocationManagerDelegate
-{
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
-    {
-        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
-        let region  = MKCoordinateRegion(center: locations[0].coordinate, span: span)
-        userMapView.setRegion(region, animated: true)
-        userMapView.showsUserLocation = true
-        
-        let annotation = MKPointAnnotation()
-        annotation.title = "Current Location"
-        userMapView.addAnnotation(annotation)
+        else
+        {
+            self.navigationBar.items?.removeAll()
+        }
     }
-}
-
-extension ViewController: resultaftersearch
-{
-    func viewListofCity(item: MKMapItem)
+    
+    
+    
+    @objc func whentappedonpho()
     {
-        arrayCity.append(item)
+        if userMapView.overlays.last != nil
+        {
+            self.userMapView.removeOverlay(userMapView.overlays.last!)
+            addpolygon = nil
+        }
         
-        //applaying annotation
-        var annotations = [MKAnnotation]()
-        
-        
-        //applaying loop
         for i in 0..<arrayCity.count
         {
-            let annotation = MKPointAnnotation()
             if i == 0
             {
-                annotation.title = "A"
+                displayRought(source: locationManager.location!.coordinate, totheDestination: arrayCity[i].placemark.coordinate, title: "A")
             }
-            else if i == 1
+            else if(i == 1)
             {
-                annotation.title = "B"
+                displayRought(source: arrayCity[i-1].placemark.coordinate, totheDestination: arrayCity[i].placemark.coordinate, title: "B")
             }
-            else if i == 2
+            else if(i == 2)
             {
-                annotation.title = "c"
+                displayRought(source: arrayCity[i-1].placemark.coordinate, totheDestination: arrayCity[i].placemark.coordinate, title: "C")
             }
-            else
-            {
-                annotation.title = "other"
-            }
-            
-            annotation.coordinate = CLLocationCoordinate2D(latitude: arrayCity[i].placemark.coordinate.latitude, longitude: arrayCity[i].placemark.coordinate.longitude)
-            
-            annotations.append(annotation)
-            
         }
-        userMapView.addAnnotations(annotations)
-        userMapView.addAllItems(in: annotations, andShow: true)
     }
     
+    
+    //this function will display the rout
     func displayRought(source : CLLocationCoordinate2D, totheDestination : CLLocationCoordinate2D, title: String)
     {
         let fromLocation = source
@@ -210,12 +148,68 @@ extension ViewController: resultaftersearch
             self.userMapView.setRegion(MKCoordinateRegion(rectt), animated: true)
 
         }
+ 
+    }
+    
+  
+    @objc func whenpressed(sender: UILongPressGestureRecognizer)
+        {
+            //alert message for displaying want to add or not ?
+            
+            let alert = UIAlertController(title: "Want to add this location", message: "Add !!", preferredStyle: .alert)
+            
+            
+            //when user will press yes than it will redirectuser to another page for searching city name
+            
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { [self] (action: UIAlertAction!)
+                in
+                let searcscreen = self.storyboard?.instantiateViewController(withIdentifier: "AddyourCityVcViewController") as! AddyourCityVcViewController
+                searcscreen.viewofMap = self.userMapView
+                searcscreen.delegate = self
+                self.navigationController?.pushViewController(searcscreen, animated: true)
+                
+            }))
+            
+            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { (action: UIAlertAction!) in }))
+            
+            present(alert, animated: true, completion: nil)
+        }
+
+   
+    
+    func insertingPolygon()
+    {
+        var points: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
         
+        for i in 0..<arrayCity.count
+        {
+            points.append(arrayCity[i].placemark.coordinate)
+        }
+        let polygon = MKPolygon(coordinates: points, count: points.count)
+        self.addpolygon = polygon
+        userMapView.addOverlay(polygon)
         
         
     }
+
+
     
-    //applay polygon
+    
+    //doubletap gesture callout
+    @objc func doubletaped(sender: UITapGestureRecognizer)
+    {
+        print("double tap gesture recognized")
+        let touchPoint = sender.location(in: userMapView)
+                        let coordinate = userMapView.convert(touchPoint, toCoordinateFrom: userMapView)
+                        let annotation = MKPointAnnotation()
+                        annotation.title = "B"
+                        annotation.coordinate = coordinate
+                        userMapView.addAnnotation(annotation)
+    }
+    
+    
+    
+   /* //applay polygon
     func applayPolygon()
     {
         var cordinatepoints: [CLLocationCoordinate2D] = [CLLocationCoordinate2D]()
@@ -229,77 +223,9 @@ extension ViewController: resultaftersearch
         self.addpolygon = polygoon
         userMapView.addOverlay(polygoon)
     }
-    
-    //calculating distance
-    
-    func distanceGetting(source : CLLocationCoordinate2D, destination : CLLocationCoordinate2D) ->  Double {
-        let coordinate₀ = CLLocation(latitude: source.latitude, longitude: source.longitude)
-        let coordinate₁ = CLLocation(latitude: destination.latitude, longitude: destination.longitude)
-
-        let distanceInMeters = coordinate₀.distance(from: coordinate₁)
-        return Double(distanceInMeters)
-    }
+    */
     
     
-    
-    func pointCheck(location : CLLocationCoordinate2D) {
-        var arrDistance : [Double] = []
-        for i in 0..<arrayCity.count
-        {
-            let dist = distanceGetting(source: location, destination: arrayCity[i].placemark.coordinate)
-            arrDistance.append(dist)
-        }
-        let ss = arrDistance.max { a, b in
-            return a > b
-        }
-        var index = 0
-        for i in 0..<arrDistance.count {
-            if ss == arrDistance[i] {
-                index = i
-                break
-            }
-        }
-        arrayCity.remove(at: index)
-//   userMapView.removeAnnotation
-        userMapView.removeAnnotations(userMapView.annotations)
-        if userMapView.overlays.last != nil {
-            userMapView.removeOverlay(userMapView.overlays.last!)
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.addAnnotations()
-        }
-        
-    }
-    
-    func addAnnotations()
-    {
-        var annotations = [MKAnnotation]()
-        
-        for i in 0..<arrayCity.count
-        {
-            let annotation = MKPointAnnotation()
-            if i == 0
-            {
-                annotation.title = "A"
-            }
-            else if i == 1
-            {
-                annotation.title = "B"
-            }
-            else if i == 2
-            {
-                annotation.title = "C"
-            }
-            else
-            {
-                annotation.title = ""
-            }
-            annotation.coordinate = CLLocationCoordinate2D(latitude: arrayCity[i].placemark.coordinate.latitude, longitude: arrayCity[i].placemark.coordinate.longitude)
-            annotations.append(annotation)
-        }
-        userMapView.addAnnotations(annotations)
-        userMapView.addAllItems(in: annotations, andShow: true)
-    }
     
     
     //after touch for three times the directions will call by using this function
@@ -327,11 +253,171 @@ extension ViewController: resultaftersearch
         }
         super.touchesEnded(touchss, with: event)
     }
+    
+    
+    
+    func pointCheck(location : CLLocationCoordinate2D) {
+        var arrDistance : [Double] = []
+        for i in 0..<arrayCity.count
+        {
+            let dist = distanceGetting(source: location, destination: arrayCity[i].placemark.coordinate)
+            arrDistance.append(dist)
+        }
+        let see = arrDistance.max { a, b in
+            return a > b
+        }
+        var index = 0
+        for i in 0..<arrDistance.count {
+            if see == arrDistance[i] {
+                index = i
+                break
+            }
+        }
+        arrayCity.remove(at: index)
+        
+        //userMapView.removeAnnotation
+        userMapView.removeAnnotations(userMapView.annotations)
+        if userMapView.overlays.last != nil {
+            userMapView.removeOverlay(userMapView.overlays.last!)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.addAnnotations()
+            self.checkforRouteOption()
+        }
+        
+    }
+    
+    
+    
+    //calculating distance
+    
+    func distanceGetting(source : CLLocationCoordinate2D, destination : CLLocationCoordinate2D) ->  Double {
+        let coordinate₀ = CLLocation(latitude: source.latitude, longitude: source.longitude)
+        let coordinate₁ = CLLocation(latitude: destination.latitude, longitude: destination.longitude)
+
+        let distanceInMeters = coordinate₀.distance(from: coordinate₁)
+        return Double(distanceInMeters)
+    }
+
+    
+    
+}
+
+
+//creating diiferent extension for differnt usages.
+
+
+//first create extenstion for viewControllar
+extension ViewController: CLLocationManagerDelegate
+{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region  = MKCoordinateRegion(center: locations[0].coordinate, span: span)
+        userMapView.setRegion(region, animated: true)
+        userMapView.showsUserLocation = true
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "Current Location"
+        userMapView.addAnnotation(annotation)
+    }
+    
+    
+    //this function will calculate how many touces happended.
+    //if there has 3 pins than it will send the data for drawing triangle
+    func addAnnotations()
+    {
+        var annotations = [MKAnnotation]()
+        
+        for i in 0..<arrayCity.count
+        {
+            let annotation = MKPointAnnotation()
+            if i == 0
+            {
+                annotation.title = "A"
+            }
+            else if i == 1
+            {
+                annotation.title = "B"
+            }
+            else if i == 2
+            {
+                annotation.title = "C"
+                insertingPolygon()
+            }
+            else
+            {
+                annotation.title = ""
+            }
+            annotation.coordinate = CLLocationCoordinate2D(latitude: arrayCity[i].placemark.coordinate.latitude, longitude: arrayCity[i].placemark.coordinate.longitude)
+            annotations.append(annotation)
+        }
+        userMapView.addAnnotations(annotations)
+        userMapView.addAllItems(in: annotations, andShow: true)
+    }
 
 }
 
 
 
+//annother extension for "resultaftersearch" that is available inside another page
+extension ViewController: resultaftersearch
+{
+    func viewListofCity(item: MKMapItem)
+    {
+        arrayCity.append(item)
+        addAnnotations()
+    }
+        
+    
+
+}
+
+
+
+//let's create another extension regarding mapViewDelegate
+
+extension ViewController : MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if addpolygon == nil {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            if overlay.title == "A" {
+                renderer.strokeColor = UIColor.blue
+            } else if overlay.title == "B" {
+                renderer.strokeColor = UIColor.red
+            } else if overlay.title == "C" {
+                renderer.strokeColor = UIColor.yellow
+            }
+            renderer.lineWidth = 4.0
+            return renderer
+        } else {
+            let renderer = MKPolygonRenderer(polygon: addpolygon!)
+            renderer.fillColor = UIColor.red.withAlphaComponent(0.50)
+            return renderer
+        }
+    }
+}
+
+
+
+//another extension for MKPolygon
+extension MKPolygon {
+    func containing(coordin: CLLocationCoordinate2D) -> Bool {
+        let polygonRenderer = MKPolygonRenderer(polygon: self)
+        let currentMapPoint: MKMapPoint = MKMapPoint(coordin)
+        let polygonViewPoint: CGPoint = polygonRenderer.point(for: currentMapPoint)
+        if polygonRenderer.path == nil {
+          return false
+        }else{
+          return polygonRenderer.path.contains(polygonViewPoint)
+        }
+    }
+}
+
+
+//another extension regarding mapview
+//this will use for displaying all the annotations
 extension MKMapView
 {
     func addAllItems()
@@ -355,7 +441,7 @@ extension MKMapView
         for annotation in annotations
         {
             let annoPoint = MKMapPoint(annotation.coordinate)
-            let rect = MKMapRect(x: annoPoint.x, y: annoPoint.y, width: 0.5, height: 0.5)
+            let rect = MKMapRect(x: annoPoint.x, y: annoPoint.y, width: 1.0, height: 1.0)
             
             if zoom.isNull
             {
@@ -375,15 +461,3 @@ extension MKMapView
 }
 
 
-extension MKPolygon {
-    func containing(coordin: CLLocationCoordinate2D) -> Bool {
-        let polygonRenderer = MKPolygonRenderer(polygon: self)
-        let currentMapPoint: MKMapPoint = MKMapPoint(coordin)
-        let polygonViewPoint: CGPoint = polygonRenderer.point(for: currentMapPoint)
-        if polygonRenderer.path == nil {
-          return false
-        }else{
-          return polygonRenderer.path.contains(polygonViewPoint)
-        }
-    }
-}
